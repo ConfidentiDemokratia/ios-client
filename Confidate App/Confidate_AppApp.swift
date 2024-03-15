c4fc6eb5f66fde3df502051c2109673898384cbd
Nikolai Trukhin <NTrukhin@alfabank.ru>
2024-03-15 15:31:57 +0000

    Initial Commit

---
+++ Confidate App/Confidate_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Confidate_AppApp.swift
+//  Confidate App
+//
+//  Created by Nikolai Trukhin on 15.03.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Confidate_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
